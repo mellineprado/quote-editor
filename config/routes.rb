6403d2e99398841f32fcdcb8e77c05b6e821a01f
melline prado <mellinevprado@hotmail.com>
2023-09-18 11:53:31 -0300

    added turbo to line item actions

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   root 'quotes#index', as: :authenticated_root
-  resources :quotes
 
   resources :quotes do
-    resources :line_item_dates, except: %i[index show] do
-      resources :line_item, except: %I[index show]
+    resources :line_item_dates, except: [:index, :show] do
+      resources :line_items, except: [:index, :show]
     end
   end
 end
