d0e9720c1a418a14c5b74b5c5919187c28e0cf87
melline prado <mellinevprado@hotmail.com>
2024-12-26 21:41:32 -0300

    adjusts navbar css

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
-  get 'line/items'
-  root to: 'pages#home'
   devise_for :users
-  root 'quotes#index', as: :authenticated_root
+
+  # Definindo a rota padrão para usuários autenticados
+  authenticated :user do
+    root 'quotes#index', as: :authenticated_root
+  end
+
+  # Rota pública para usuários não autenticados
+  root 'pages#home'
 
   resources :quotes do
-    resources :line_item_dates, except: [:index, :show] do
-      resources :line_items, except: [:index, :show]
+    resources :line_item_dates, except: %i[index show] do
+      resources :line_items, except: %i[index show]
     end
   end
 end
